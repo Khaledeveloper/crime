6e5054c0214f731149c5b9d431b8bb4492c8eb75
Your Name <you@example.com>
2017-10-16 17:09:25 +0200

    crimelap

---
+++ app/src/main/java/com/example/khaled/crime/CrimeLab.java
@@ -0,0 +1,12 @@
+package com.example.khaled.crime;
+
+import java.util.ArrayList;
+
+/**
+ * Created by khaled on 16/10/2017.
+ */
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    //private ArrayList<Crime> mCrimes;
+}
